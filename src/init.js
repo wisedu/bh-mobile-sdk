edc1ef209a15a7f91cd966ce61d12133abf1d5de
GilbertSun <szb4321@gmail.com>
2016-09-09 14:50:52 +0800

    add init function

--- src/init.js
+++ src/init.js
@@ -1,3 +1,26 @@
 export default () => {
-  console.log('init')
+  function injectScript(url) {
+    var script = document.createElement("script");
+    script.src = url;
+    document.head.appendChild(script);
+  }
+
+  function init(){
+
+    var pathPrefix = "mamp://injectionmamp/";
+    if (pathPrefix === null) {
+      console.log('Could not find mamp.js script tag. Plugin loading may fail.');
+      pathPrefix = '';
+    }
+    injectScript(pathPrefix + 'cordova.js');
+  }
+
+  init();
+  window.close = function(){
+    window.location.href = 'mamp://close';
+  };
+  window.mamp = {};
+  window.mamp.initEnv = function(callback){
+    document.addEventListener("deviceready", callback, false);
+  };
 }
