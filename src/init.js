aa9ef54779b91900e383e5dba30d05ec099707b5
GilbertSun <szb4321@gmail.com>
2016-10-28 10:23:09 +0800

    add sdk debug for browser

--- src/init.js
+++ src/init.js
@@ -2,21 +2,24 @@ let INIT = false
 
 export default (callback) => {
   if (INIT) {
-    alert('请不要重复初始化')
+    console.error('请不要重复初始化')
     return
   }
+  if (localStorage.getItem('wisedu-browser-debug')) {
+    callback()
+  } else {
+    let distUrl = 'mamp://injectionmamp/cordova.js' // 远端文件地址
 
-  let distUrl = 'mamp://injectionmamp/cordova.js' // 远端文件地址
+    var script = document.createElement("script")
+    script.src = distUrl
+    document.head.appendChild(script)
 
-  var script = document.createElement("script")
-  script.src = distUrl
-  document.head.appendChild(script)
+    window.close = function() {
+      window.location.href = 'mamp://close';
+    }
 
-  window.close = function(){
-    window.location.href = 'mamp://close';
+    document.addEventListener("deviceready", callback, false);
   }
-
-  document.addEventListener("deviceready", callback, false);
 
   INIT = true
 }
